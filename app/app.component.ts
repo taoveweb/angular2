55dc1e9c83dcf9ff2ea9c1dcf0f732a9a49a5f30
taoveweb <taoveweb@gmail.com>
2016-05-09 14:04:11 +0800

    all

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,30 +1,34 @@
-import { Component } from '@angular/core';
-import { Hero } from './hero';
-
+import { Component, OnInit } from '@angular/core';
+import { Routes, Router, ROUTER_DIRECTIVES } from '@angular/router';
+import { CrisisCenterComponent } from './crisis-center/crisis-center.component';
+import { HeroListComponent }     from './heroes/hero-list.component';
+import { HeroDetailComponent }   from './heroes/hero-detail.component';
+import { FuckComponent }   from './fuck/fuck.component';
+import { DialogService }         from './dialog.service';
+import { HeroService }           from './heroes/hero.service';
 @Component({
     selector: 'my-app',
     template: `
-    <h1>{{title}}</h1>
-    <input #box (keyup.enter)="onKey(box.value)"/>
-    <p>{{values}}</p>
-    <h2 (click)="onClickMe($event)">my favorite hero is :{{myhero}}</h2>
-    <ul>
-    <li *ngFor="#hero of heroes">{{hero.name}}</li>
-    </ul>
-    `
+    <h1 class="title">Component Router</h1>
+    <nav>
+      <a [routerLink]="['/crisis-center']">Crisis Center</a>
+      <a [routerLink]="['/heroes']">Heroes</a>
+      <a [routerLink]="['/fuck']">fuck</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
+    providers:  [DialogService, HeroService],
+    directives: [ROUTER_DIRECTIVES]
 })
-export class AppComponent {
-    title='Tour of heroes';
-    myhero="windstorm";
-    values="";
-    heroes = [
-        new Hero(1,'windstorm'),
-        new Hero(13,'bombato'),
-        new Hero(15,'mageta'),
-        new Hero(16,'tornado'),
-    ];
-
-    onKey(value:string){
-        this.values+=value;
+@Routes([
+    {path: '/crisis-center',  component: CrisisCenterComponent},
+    {path: '/heroes',  component: CrisisCenterComponent},
+    {path: '/fuck',  component: FuckComponent},
+    {path: '/hero/:id', component: HeroDetailComponent},
+])
+export class AppComponent implements OnInit {
+    constructor(private router: Router) {}
+    ngOnInit() {
+        this.router.navigate(['/crisis-center']);
     }
 }
---
+++ app/crisis-center/crisis-center.component.ts
@@ -0,0 +1,18 @@
+import { Component }     from '@angular/core';
+import { ROUTER_DIRECTIVES, Routes } from '@angular/router';
+import { CrisisDetailComponent } from './crisis-detail.component';
+import { CrisisListComponent }   from './crisis-list.component';
+import { CrisisService }         from './crisis.service';
+@Component({
+    template:  `
+    <h2>CRISIS CENTER</h2>
+    <router-outlet></router-outlet>
+  `,
+    directives: [ROUTER_DIRECTIVES],
+    providers:  [CrisisService]
+})
+@Routes([
+    {path: '',    component: CrisisListComponent}, // , useAsDefault: true}, // coming soon
+    {path: '/:id', component: CrisisDetailComponent}
+])
+export class CrisisCenterComponent { }
